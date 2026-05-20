1ade394a7990eaafc625119607b329c25b6b89fb
Daniel Van Bueren <daniel.vanbueren@proton.me>
2026-05-20 11:17:17 -0500

    Create buildInfo.js

---
+++ airspace-sim/app/buildInfo.js
@@ -0,0 +1,43 @@
+export default function buildInfo() {
+    // descriptors
+    const projectName = 'Airspace Simulator'
+    const githubRepoName = 'airspace-sim'
+    const githubRepoOwner = 'danvanbueren'
+
+    // links
+    const githubOwnerLink = `https://github.com/${githubRepoOwner}`
+    const githubRepoLink = `https://github.com/${githubRepoOwner}/${githubRepoName}`
+    const githubIssuesLink = `https://github.com/${githubRepoOwner}/${githubRepoName}/issues`
+    const parrotSourLink = 'https://parrotsour.com/'
+
+    // version info
+    const versionEdition = '2026'
+    const versionMajor = '0'
+    const versionMinor = '3'
+    const versionFlag = 'indev'
+
+    // full version
+    const fullyQualifiedVersion =
+        versionEdition + '.' + versionMajor + '.' + versionMinor + '-' + versionFlag
+
+    // copyright notice
+    const copyrightTitle = 'COPYRIGHT © 2026 DANIEL VAN BUEREN. ALL RIGHTS RESERVED.'
+    const copyrightDescription = 'THIS MATERIAL IS PROTECTED BY COPYRIGHT LAW. NO PART OF THIS WORK MAY BE COPIED, REPRODUCED, DISTRIBUTED, TRANSMITTED, DISPLAYED, OR PERFORMED IN ANY FORM OR BY ANY MEANS, ELECTRONIC, MECHANICAL, PHOTOCOPYING, RECORDING, OR OTHERWISE, WITHOUT PRIOR WRITTEN PERMISSION FROM THE COPYRIGHT OWNER.'
+
+    return ({
+        projectName,
+        githubRepoName,
+        githubRepoOwner,
+        githubOwnerLink,
+        githubRepoLink,
+        githubIssuesLink,
+        parrotSourLink,
+        versionEdition,
+        versionMajor,
+        versionMinor,
+        versionFlag,
+        fullyQualifiedVersion,
+        copyrightTitle,
+        copyrightDescription,
+    })
+}
