6704b564d80aaaedae572f8abfe6a2cc8bc5726c
Daniel Van Bueren <daniel.vanbueren@proton.me>
2026-07-10 14:48:42 -0500

    Pull project name from package.json

--- airspace-sim/app/layout.js
+++ airspace-sim/app/layout.js
@@ -4,7 +4,7 @@ import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
-import {cookies} from 'next/headers'
+import { cookies } from 'next/headers'
 import CustomThemeContext from './contexts/CustomThemeContext'
 import {
     CONTROL_BINDINGS_COOKIE_NAME, ControlBindingsProvider,
@@ -17,19 +17,24 @@ import {
     ACTION_PANELS_COOKIE_NAME,
     ActionPanelsProvider,
 } from './contexts/ActionPanelsContext'
-import {FloatingDraggableStackProvider} from './contexts/FloatingDraggableStackContext'
-import {DrawToolsProvider} from './contexts/DrawToolsContext'
-import {DrawGeometryProvider} from './contexts/DrawGeometryContext'
-import {UseGlobalInteractionGuards} from '@/app/hooks/global/useGlobalInteractionGuards'
-import {THEME_COOKIE_NAME} from '@/app/contexts/CustomThemeContext'
-import {MapStateProvider} from './contexts/MapStateContext'
-import {SensorDisplayProvider} from './contexts/SensorDisplayContext'
-import {SimulationProvider} from './contexts/SimulationContext'
-import {PerformanceMonitorProvider} from './contexts/PerformanceMonitorContext'
+import { FloatingDraggableStackProvider } from './contexts/FloatingDraggableStackContext'
+import { DrawToolsProvider } from './contexts/DrawToolsContext'
+import { DrawGeometryProvider } from './contexts/DrawGeometryContext'
+import { UseGlobalInteractionGuards } from '@/app/hooks/global/useGlobalInteractionGuards'
+import { THEME_COOKIE_NAME } from '@/app/contexts/CustomThemeContext'
+import { MapStateProvider } from './contexts/MapStateContext'
+import { SensorDisplayProvider } from './contexts/SensorDisplayContext'
+import { SimulationProvider } from './contexts/SimulationContext'
+import { PerformanceMonitorProvider } from './contexts/PerformanceMonitorContext'
 
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/next"
-import {PROJECT_NAME} from './config/projectName'
+import packageJson from '../package.json'
+
+const PROJECT_NAME = packageJson.name
+    .split('-')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ')
 
 export const dynamic = 'force-dynamic'
 
@@ -55,7 +60,7 @@ export const viewport = {
     initialScale: 1,
 }
 
-export default async function RootLayout({children}) {
+export default async function RootLayout({ children }) {
     const cookieStore = await cookies()
     const themeCookie = cookieStore.get(THEME_COOKIE_NAME)?.value
     const controlBindingsCookie = cookieStore.get(CONTROL_BINDINGS_COOKIE_NAME)?.value
@@ -63,34 +68,34 @@ export default async function RootLayout({children}) {
     const actionPanelsCookie = cookieStore.get(ACTION_PANELS_COOKIE_NAME)?.value
 
     return (<html lang='en'>
-    <body>
-    <MapStateProvider>
-        <UseGlobalInteractionGuards>
-            <CustomThemeContext initialMode={themeCookie}>
-                <AppSettingsProvider initialSettings={appSettingsCookie}>
-                    <ActionPanelsProvider initialActionPanels={actionPanelsCookie}>
-                        <DrawToolsProvider>
-                        <DrawGeometryProvider>
-                        <FloatingDraggableStackProvider>
-                            <SensorDisplayProvider>
-                            <SimulationProvider>
-                                <PerformanceMonitorProvider>
-                                    <ControlBindingsProvider initialBindings={controlBindingsCookie}>
-                                        {children}
-                                    </ControlBindingsProvider>
-                                </PerformanceMonitorProvider>
-                            </SimulationProvider>
-                        </SensorDisplayProvider>
-                        </FloatingDraggableStackProvider>
-                        </DrawGeometryProvider>
-                        </DrawToolsProvider>
-                    </ActionPanelsProvider>
-                </AppSettingsProvider>
-            </CustomThemeContext>
-        </UseGlobalInteractionGuards>
-    </MapStateProvider>
-    </body>
-    <SpeedInsights />
-    <Analytics />
+        <body>
+            <MapStateProvider>
+                <UseGlobalInteractionGuards>
+                    <CustomThemeContext initialMode={themeCookie}>
+                        <AppSettingsProvider initialSettings={appSettingsCookie}>
+                            <ActionPanelsProvider initialActionPanels={actionPanelsCookie}>
+                                <DrawToolsProvider>
+                                    <DrawGeometryProvider>
+                                        <FloatingDraggableStackProvider>
+                                            <SensorDisplayProvider>
+                                                <SimulationProvider>
+                                                    <PerformanceMonitorProvider>
+                                                        <ControlBindingsProvider initialBindings={controlBindingsCookie}>
+                                                            {children}
+                                                        </ControlBindingsProvider>
+                                                    </PerformanceMonitorProvider>
+                                                </SimulationProvider>
+                                            </SensorDisplayProvider>
+                                        </FloatingDraggableStackProvider>
+                                    </DrawGeometryProvider>
+                                </DrawToolsProvider>
+                            </ActionPanelsProvider>
+                        </AppSettingsProvider>
+                    </CustomThemeContext>
+                </UseGlobalInteractionGuards>
+            </MapStateProvider>
+        </body>
+        <SpeedInsights />
+        <Analytics />
     </html>)
 }
